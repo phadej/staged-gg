6fc459616bdb1ed6d49001e82b81fe25a0a9026b
David Feuer <David.Feuer@gmail.com>
2022-01-24 12:10:49 -0500

    Fix Foo

--- staged-gg-examples/src/Staged/GHC/Generics/Examples/Foo.hs
+++ staged-gg-examples/src/Staged/GHC/Generics/Examples/Foo.hs
@@ -9,15 +9,12 @@ module Staged.GHC.Generics.Examples.Foo where
 import qualified GHC.Generics as GHC
 
 import Staged.GHC.Generics
-import Staged.GHC.Generics.GHCish
 
 -- | Example product type.
 data Foo = Foo [Int] Ordering String
   deriving (GHC.Generic)
 
-instance Generic Foo where
-    to   = ghcGenericTo
-    from = ghcGenericFrom
+instance Generic Foo
 
 data Bar f a = Bar (f a)
   deriving (GHC.Generic, GHC.Generic1)
